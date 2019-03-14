92339aadf861103d138b4009575e8c2683e6c92d
meiichin <meiichan3017@gmail.com>
2019-03-14 23:08:31 +0700

    [Modified] Home page arter login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
+<!-- home.blade.php -->
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  @if(\Session::has('error'))
+    <div class="alert alert-danger">
+      {{\Session::get('error')}}
+    </div>
+  @endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="panel-body">
+                    <a href="{{url('admin')}}">Admin</a>
                 </div>
             </div>
         </div>
